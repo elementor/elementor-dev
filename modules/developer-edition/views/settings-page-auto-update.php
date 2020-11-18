c5733a9443eb9849245a16df59bc1536bf56e66a
Nevo Golan <nevog@elementor.com>
2020-11-18 09:36:57 +0200

    Modules/Developer-Edition: Change - Manage auto update throw wordpress base auto update.
    ref: ED-779

--- modules/developer-edition/views/settings-page-auto-update.php
+++ modules/developer-edition/views/settings-page-auto-update.php
@@ -1,6 +1,16 @@
 <?php
+
+use ElementorDev\Bootstrap;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
+}
+
+$is_checked = false;
+$auto_updates = (array) get_site_option( 'auto_update_plugins', [] );
+
+if ( in_array( Bootstrap::ELEMENTOR_PLUGIN_NAME, $auto_updates, true ) ) {
+	$is_checked = true;
 }
 ?>
 <label for="elementor-dev-auto-update">
@@ -9,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 		id="elementor_dev_auto_update"
 		name="elementor_dev_auto_update"
 		value="yes"
-		<?php checked( get_option( 'elementor_dev_auto_update', false ), 'yes' ); ?>
+		<?php checked( $is_checked ); ?>
 	/>
 	<?php esc_html_e( 'Activate Auto Updates', 'elementor-dev' ); ?>
 </label>
